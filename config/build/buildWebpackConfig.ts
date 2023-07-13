a616409eb38a12417d880ba8ab43c7187115c04c
Tsuker <igorek888rane@gmail.com>
2023-07-13 11:57:04 +0300

    feat:фсд,абсолютные пути

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -19,7 +19,7 @@ export function buildWebpackConfig(options:BuildOptions):Configuration {
         module: {
             rules: buildLoaders(options)
         },
-        resolve: buildResolves(),
+        resolve: buildResolves(options),
         devtool: isDev?'inline-source-map':undefined,
         devServer:isDev?buildDevServer(options):undefined
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import {render} from "react-dom";
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
+import App from "./app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
-import './styles/index.scss'
-import ThemeProvider from "./theme/ThemeProvider";
+import './app/styles/index.scss'
+
 
 render(
    <BrowserRouter>
